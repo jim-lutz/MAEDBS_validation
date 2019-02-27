b0830a3e8c586a92412d6c4aa93390e3dfa94c02
Jim Lutz <jdlutz@hotwaterresearch.net>
2019-02-26 16:39:40 -0800

    cleaned up working directory commands

--- load_WARS.R
+++ load_WARS.R
@@ -7,8 +7,18 @@
 # set packages & etc
 source("setup.R")
 
-# set up paths to working directories
-wd_WARS <- "/home/jiml/HotWaterResearch/projects/CEC Appliance Standards/2019-02-25 validation/"
+# path to data
+wd_WARS <- paste0(dirname(getwd()),"/2019-02-25 validation/")
+
+# data directory
+if(!dir.exists("./data")){
+  dir.create("./data")
+}
+
+# charts directory
+if(!dir.exists("./charts")){
+  dir.create("./charts")
+}
 
 # read '2019-02-26 Residential Duty Commercial Water Heaters (WARS).csv'
 DT_WARS <-
@@ -16,41 +26,28 @@ DT_WARS <-
                       "2019-02-26 Residential Duty Commercial Water Heaters (WARS).xls"))
 
 # see what's there
-length(names(DT_RECS))
-# [1] 940
-nrow(DT_RECS)
-# [1] 12083
+length(names(DT_WARS))
+# [1] 18
+nrow(DT_WARS)
+# [1] 205
+names(DT_WARS)
+#  [1] "Manufacturer"                                                                      
+#  [2] "Brand"                                                                             
+#  [3] "Model Number"                                                                      
+#  [4] "Regulatory Status"                                                                 
+#  [5] "Energy Source"                                                                     
+#  [6] "Rated Volume"                                                                      
+#  [7] "Measured Volume"                                                                   
+#  [8] "Input Rating"                                                                      
+#  [9] "Heat Traps? (T/F)"                                                                 
+# [10] "Ozone Depleting Substance in Insulation? (T/F)"                                    
+# [11] "Ozone Depleting Substance in Refrigerant (for heat pump water heaters only)? (T/F)"
+# [12] "Constant burning pilot light? (T/F)"                                               
+# [13] "Mobile Home? (T/F)"                                                                
+# [14] "Water Heater Type"                                                                 
+# [15] "Draw Pattern"                                                                      
+# [16] "Uniform Energy Factor"                                                             
+# [17] "Uniform Energy Factor Standard"                                                    
+# [18] "Add Date"                                                                          
 
-# read the recs2009_public_repweights.csv
-DT_RECS_repweights <-
-  fread(file = paste0(wd_RECS,"recs2009_public_repweights.csv"))
-
-# see what's there
-length(names(DT_RECS_repweights))
-# [1] 246
-nrow(DT_RECS_repweights)
-# [1] 12083
-
-# merge on DOEID
-DT_RECS <-
-  merge(DT_RECS, DT_RECS_repweights, by='DOEID')
-
-# see what's there
-length(names(DT_RECS))
-# [1] 1185
-nrow(DT_RECS)
-# [1] 12083
-
-# keep only California data
-DT_RECS_CA <-
-  DT_RECS[ REPORTABLE_DOMAIN==26,]
-
-# see what's there
-length(names(DT_RECS_CA))
-# [1] 1185
-nrow(DT_RECS_CA)
-# [1] 1606
-
-# save as an .Rdata file
-save(DT_RECS_CA, file = "data/DT_RECS_CA.Rdata")
 
